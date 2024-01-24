4591a512ec25eb0faf8da76a55c8fba724ad4276
Samuel Afari <afarisamuel82@gmail.com>
2024-01-24 13:16:54 +0000

    Preparations for full backend

---
+++ myApp/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Questions(models.Model):
+    question = models.TextField()
+    answer = models.TextField()
+
+    def __str__(self) -> str:
+        return (self.question)
