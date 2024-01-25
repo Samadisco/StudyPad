3489d5c6de4b6093d73834b755960f505bde1242
Samuel Afari <afarisamuel82@gmail.com>
2024-01-25 07:39:37 +0000

    Migrating Kanski table

--- myApp/models.py
+++ myApp/models.py
@@ -10,11 +10,14 @@ class Questions(models.Model):
     def __str__(self) -> str:
         return (self.question)
     
-class Kanski(models.Model):
+class Kanski(models.Model,):
     question = models.TextField(blank=True, null=True)
     image = models.ImageField(upload_to='Kanski/', null=True, blank=True)
     chapter = models.CharField(max_length=50,blank=True, null=True)
     answer = models.TextField(blank=True, null=True)
+    
+    class Meta:
+        verbose_name_plural = "Kanski"
 
     def __str__(self) -> str:
         return (self.question)    
