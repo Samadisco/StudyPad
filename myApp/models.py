7ab6074a0ff64ce0cead5e6b40bdad2e343cefab
Samuel Afari <afarisamuel82@gmail.com>
2024-01-25 07:32:21 +0000

    Adding Kanski class as a model

--- myApp/models.py
+++ myApp/models.py
@@ -9,3 +9,12 @@ class Questions(models.Model):
 
     def __str__(self) -> str:
         return (self.question)
+    
+class Kanski(models.Model):
+    question = models.TextField(blank=True, null=True)
+    image = models.ImageField(upload_to='Kanski/', null=True, blank=True)
+    chapter = models.CharField(max_length=50,blank=True, null=True)
+    answer = models.TextField(blank=True, null=True)
+
+    def __str__(self) -> str:
+        return (self.question)    
